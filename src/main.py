70c942ee85961bb56573d096b3fd702c511235ef
mojonapower <maca95@gmail.com>
2022-01-22 11:56:42 -0300

    read json file and exception management done

--- src/main.py
+++ src/main.py
@@ -2,22 +2,38 @@
 import sys
 import os
 
-
-def readJsonFile(file):
-    try:
-        while(True):
-            f=open(file)
+def readJsonFile():
+    while(True):
+        try:
+            ruta='entrada/'
+            file=input('Por favor ingrese el nombre del archivo de entrada a utilizar, recuerde indicar la extension del archivo')
+            f=open(ruta+file)
             body = f.readline()
             print("contenido de archivo encontrado \n", body)
+            answer = input("Es correcto el cuerpo del archivo? Escriba si / no ")
+            print(answer)
+            if(answer.lower()=='si'):
+                print("Perfecto")
+                break
+            if(answer.lower()=='no'):
+                print("analizaremos nuevamente ...")
+                    
+            if(answer.lower() != 'si' or answer.lower() != 'no'):
+                print("valor inesperado, intentelo nuevamente")
+            
 
-    except OSError as err:
-        print("OS error: {0}".format(err))
-    except BaseException as err:
-        print(f"Unexpected {err=}, {type(err)=}")
-        raise
+        #manejo de excepciones iniciales
+        except OSError as err:
+            print("OS error: {0}".format(err))
+        except BaseException as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+            raise
+        except NameError(dato):
+            print('An exception flew by!, you entered',dato, "but it's not expected" )
+            raise
 
 
 
 if __name__ == "__main__":
-    readJsonFile('entrada/entrada.json')
+    readJsonFile()
 
